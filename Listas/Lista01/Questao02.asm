592d2faa534c844b9484d13e975cf4b4af03005e
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-10-13 13:02:22 -0300

    Create Questao02.asm

---
+++ Listas/Lista01/Questao02.asm
@@ -0,0 +1,45 @@
+.text
+main:
+	add $4, $0, 'h'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'e'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'l'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'l'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'o'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, ' '
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'w'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'o'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'r'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'l'
+	addi $2, $0, 11
+	syscall
+	
+	add $4, $0, 'd'
+	addi $2, $0, 11
+	syscall
