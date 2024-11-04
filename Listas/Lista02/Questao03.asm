44a86444bc6161ce99b6deb7cb9a07e55c31a6e0
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-11-04 19:12:00 +0000

    refazendoDiferente

--- Listas/Lista02/Questao03.asm
+++ Listas/Lista02/Questao03.asm
@@ -9,22 +9,19 @@ main:
 	add $9, $0, $2
 	
 	slt $10, $8, $9
-	beq $10, $0, primeiro
+	beq $10, $0, maiorA
 	add $4, $0, $9
-	j print
-primeiro:
+	sub $11, $9, $8
+	j fim
+maiorA:
 	add $4, $0, $8
-print:
+	sub $11, $8, $9
+fim:
 	addi $2, $0, 1
 	syscall
 	add $4, $0, '\n'
 	addi $2, $0, 11
 	syscall
-diferença:
-	sub $4, $8, $9
-print2:
+	add $4, $0, $11
 	addi $2, $0, 1
 	syscall
-fim:	
-	addi $2, $0, 10
-	syscall	
